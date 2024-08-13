92c9b153553109409b9b39919cf32fefd7f5680f
Harish718k <harishbca718@gmail.com>
2024-08-13 19:36:48 +0530

    Feature: Added LocalStorage to store score values

--- script.js
+++ script.js
@@ -16,15 +16,27 @@ const scoreBoard2 = document.getElementById("computerScore");
 const resetBtn = document.getElementById("resetBtn");
 
 function loadEventListener() {
-    // document.addEventListener("DOMContentLoaded", getItem);
-    resetBtn.addEventListener("click", clearScore);
+  // Load scores from localStorage when the page loads
+  loadScores();
+  resetBtn.addEventListener("click", clearScore);
 }
 
+function loadScores() {
+  const storedUserScore = localStorage.getItem('userScore');
+  const storedComputerScore = localStorage.getItem('computerScore');
 
+  if (storedUserScore !== null) {
+    userScore = parseInt(storedUserScore);
+  }
+  if (storedComputerScore !== null) {
+    computerScore = parseInt(storedComputerScore);
+  }
 
+  updateScore();
+}
 
 function computerMove() {
-    let random = Math.floor(Math.random()*10+1);
+  let random = Math.floor(Math.random()*10+1);
     if(random >= 1 && random < 4) {
         computer = "rock";
     } 
@@ -44,110 +56,93 @@ function computerMove() {
     else{
         screen2.src = "./img/scissor.png";
     }
-    
 }
 
-
 function userMove() {
-    userChoice.forEach(choice => {
-        choice.addEventListener("click", () => {
-            user = choice.alt;
-            if(user === "rock") {
-                screen1.src = "./img/rock.png";
-            } 
-            else if(user === "paper") {
-                screen1.src = "./img/paper.png";
-            }
-            else{
-                screen1.src = "./img/scissor.png";
-            }
-            computerMove();
-            winner();
-            updateScore();
-        });
-    })
+  userChoice.forEach(choice => {
+    choice.addEventListener("click", () => {
+        user = choice.alt;
+        if(user === "rock") {
+            screen1.src = "./img/rock.png";
+        } 
+        else if(user === "paper") {
+            screen1.src = "./img/paper.png";
+        }
+        else{
+            screen1.src = "./img/scissor.png";
+        }
+        computerMove();
+        winner();
+        updateScore();
+    });
+})
 }
 
 function winner() {
-    if(user === "rock"){
-        if(computer === "rock") {
-            result = "Tie!";
-        }
-        else if(computer === "paper"){
-            result = "You Lose!";
-            computerScore +=1;
-        }
-        else if(computer === "scissor") {
-            result = "You Won!";
-            userScore += 1;
-        }
+  if(user === "rock"){
+    if(computer === "rock") {
+        result = "Tie!";
     }
-    else if(user === "paper") {
-        if(computer === "rock") {
-            result = "You Won!";
-            userScore += 1;
-        }
-        else if(computer === "paper"){
-            result = "Tie!";
-        }
-        else if(computer === "scissor") {
-            result = "You Lose!";
-            computerScore +=1;
-        }
+    else if(computer === "paper"){
+        result = "You Lose!";
+        computerScore +=1;
     }
+    else if(computer === "scissor") {
+        result = "You Won!";
+        userScore += 1;
+    }
+}
+else if(user === "paper") {
+    if(computer === "rock") {
+        result = "You Won!";
+        userScore += 1;
+    }
+    else if(computer === "paper"){
+        result = "Tie!";
+    }
+    else if(computer === "scissor") {
+        result = "You Lose!";
+        computerScore +=1;
+    }
+}
 
-    else if(user === "scissor") {
-        if(computer === "rock") {
-            result = "You Lose!";
-            computerScore +=1;
-        }
-        else if(computer === "paper"){
-            result = "You Won!";
-            userScore += 1;
-        }
-        else if(computer === "scissor") {
-            result = "Tie!";
-        }
+else if(user === "scissor") {
+    if(computer === "rock") {
+        result = "You Lose!";
+        computerScore +=1;
     }
-    resultBoard.textContent = result;
+    else if(computer === "paper"){
+        result = "You Won!";
+        userScore += 1;
+    }
+    else if(computer === "scissor") {
+        result = "Tie!";
+    }
+}
+resultBoard.textContent = result;
 }
 
 function updateScore() {
-    scoreBoard1.innerHTML = `
+  scoreBoard1.innerHTML = `
     <p>You</p>
     <h1>${userScore}</h1>
-    `;
+  `;
 
-    scoreBoard2.innerHTML = `
+  scoreBoard2.innerHTML = `
     <p>Computer</p>
-    <h1 >${computerScore}</h1>
-    `;
+    <h1>${computerScore}</h1>
+  `;
 
-    localStorage.setItem("userScore", userScore);
-    localStorage.setItem("computerScore", computerScore);
+  localStorage.setItem("userScore", userScore);
+  localStorage.setItem("computerScore", computerScore);
 }
 
-// function getItem() {
-//     console.log(localStorage.getItem("userScore"))
-
-//     scoreBoard1.innerHTML = `
-//     <p>You</p>
-//     <h1>${userScore}</h1>
-//     `;
-
-//     scoreBoard2.innerHTML = `
-//     <p>Computer</p>
-//     <h1 >${computerScore}</h1>
-//     `;
-// }
-
 function clearScore() {
-    userScore = 0;
-    computerScore = 0;
-    updateScore();
-    resultBoard.textContent = "Start!";
+  userScore = 0;
+  computerScore = 0;
+  updateScore();
+  resultBoard.textContent = "Start!";
 }
 
 loadEventListener();
-updateScore();
 userMove();
